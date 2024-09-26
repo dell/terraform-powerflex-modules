79ad3229390033136afe45eaff798e109dfc5de5
Luis Liu <luis_liu@dell.com>
2024-09-26 11:25:24 +0800

    init
    Signed-off-by: Luis Liu <luis_liu@dell.com>

---
+++ examples/azure_core/variables.tf
@@ -0,0 +1,96 @@
+# /*
+# Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+# Licensed under the Mozilla Public License Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://mozilla.org/MPL/2.0/
+
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# */
+
+variable "username" {
+  type        = string
+  default     = "admin"
+  description = "PowerFlex manamgement platform portal username"
+}
+
+variable "password" {
+  type        = string
+  description = "PowerFlex manamgement platform portal password"
+}
+
+variable "endpoint" {
+  type        = string
+  description = "PowerFlex manamgement platform endpoint"
+}
+
+variable "insecure" {
+  type    = bool
+  default = true
+}
+
+variable "login_credential" {
+  type = object({
+    username = string
+    password = string
+  })
+  description = "SSH login credential for the cluster nodes"
+}
+
+## We will maintain the latest version as the default version
+## Use for PowerFlex 4.6 build. PowerFlex 4.5.2.1 Build 105 30 May 2024
+variable "packages" {
+  type = list(string)
+  default = [
+    "/root/PowerFlex_4.5.2100.105_SLES15.4/EMC-ScaleIO-lia-4.5-2100.105.sles15.4.x86_64.rpm",
+    "/root/PowerFlex_4.5.2100.105_SLES15.4/EMC-ScaleIO-mdm-4.5-2100.105.sles15.4.x86_64.rpm",
+    "/root/PowerFlex_4.5.2100.105_SLES15.4/EMC-ScaleIO-sdr-4.5-2100.105.sles15.4.x86_64.rpm",
+    "/root/PowerFlex_4.5.2100.105_SLES15.4/EMC-ScaleIO-sds-4.5-2100.105.sles15.4.x86_64.rpm",
+    "/root/PowerFlex_4.5.2100.105_SLES15.4/EMC-ScaleIO-sdt-4.5-2100.105.sles15.4.x86_64.rpm",
+    "/root/PowerFlex_4.5.2100.105_SLES15.4/EMC-ScaleIO-activemq-5.18.3-68.noarch.rpm"
+  ]
+}
+
+variable "sds_ips" {
+  type = list(string)
+}
+
+variable "protection_domain" {
+  type    = string
+  default = "PD1"
+}
+
+variable "storage_pool" {
+  type    = string
+  default = "SP1"
+}
+
+variable "data_disk_count" {
+  type    = number
+  default = 20
+  validation {
+    condition = (
+      var.data_disk_count >= 1 &&
+      var.data_disk_count <= 24
+    )
+    error_message = "Must be between 1 and 24."
+  }
+}
+
+variable "is_multi_az" {
+  type    = bool
+  default = false
+}
+
+variable "is_balanced" {
+  type        = bool
+  default     = true
+  description = "Deployment type, true for balanced type, false for optimized type"
+}
---
+++ examples/azure_pfmp/main.tf
@@ -0,0 +1,47 @@
+# /*
+# Copyright (c) 2024 Dell Inc., or its subsidiaries. All Rights Reserved.
+
+# Licensed under the Mozilla Public License Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://mozilla.org/MPL/2.0/
+
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# */
+
+module "azure_pfmp" {
+  source = "../../modules/azure_pfmp"
+  cluster = {
+    node_count        = var.cluster_node_count
+    is_multi_az       = var.is_multi_az
+    deployment_type   = var.deployment_type
+    data_disk_count   = var.data_disk_count
+    data_disk_size_gb = var.data_disk_size_gb
+  }
+  data_disk_logical_sector_size = var.data_disk_logical_sector_size
+  enable_bastion                = var.enable_bastion
+  enable_jumphost               = var.enable_jumphost
+  existing_resource_group       = var.existing_resource_group
+  location                      = var.location
+  powerflex_version             = var.powerflex_version
+  prefix                        = var.prefix
+  ssh_key                       = var.ssh_key
+}
+
+output "bastion_tunnel" {
+  value = module.azure_pfmp.bastion_tunnel != null ? "az network bastion tunnel --name ${module.azure_pfmp.bastion_tunnel.bastion_name} --resource-group ${module.azure_pfmp.bastion_tunnel.resource_group} --target-resource-id ${module.azure_pfmp.bastion_tunnel.installer_id} --resource-port 22 --port 1111" : null
+}
+
+output "pfmp_ip" {
+  value = module.azure_pfmp.pfmp_ip
+}
+
+output "sds_nodes" {
+  value = module.azure_pfmp.sds_nodes[*].ip
+}
